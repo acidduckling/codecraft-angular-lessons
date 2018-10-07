bd305e1cb2c3eb95e73e58e8a1e1c089bbab4a5a
Clayton Wakefield <acidduckling@gmail.com>
2018-10-07 22:42:12 +1100

    view and content children (and lifecycle hooks)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Joke } from './classes/joke';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {}
+export class AppComponent {
+  joke: Joke = new Joke(
+    'A kid threw a lump of cheddar at me',
+    'I thought ‘That’s not very mature’'
+  );
+}
--- src/app/components/joke-list/joke-list.component.ts
+++ src/app/components/joke-list/joke-list.component.ts
@@ -1,23 +1,61 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+  ElementRef,
+  ContentChild,
+  AfterContentInit
+} from '@angular/core';
 import { Joke } from '../../classes/joke';
+import { JokeComponent } from '../joke/joke.component';
 
 @Component({
   selector: 'app-joke-list',
   templateUrl: './joke-list.component.html',
   styles: []
 })
-export class JokeListComponent implements OnInit {
-  jokes: Joke[];
+export class JokeListComponent implements OnInit, AfterViewInit, AfterContentInit {
+  @ViewChild(JokeComponent)
+  jokeViewChild: JokeComponent;
+
+  @ViewChildren(JokeComponent)
+  jokeViewChildren: QueryList<JokeComponent>;
+
+  @ViewChild('header')
+  headerEl: ElementRef;
+
+  @ContentChild(JokeComponent)
+  jokeContentChild: JokeComponent;
+
+  jokes = [
+    new Joke('What did the cheese say when it looked in the mirror?', 'Hello-Me (Halloumi)'),
+    new Joke(
+      'What kind of cheese do you use to disguise a small horse?',
+      'Mask-a-pony (Mascarpone)'
+    )
+  ];
 
   constructor() {
-    this.jokes = [
-      new Joke('What did the cheese say when it looked in the mirror?', 'Hello-Me (Halloumi)'),
-      new Joke(
-        'What kind of cheese do you use to disguise a small horse?',
-        'Mask-a-pony (Mascarpone)'
-      ),
-      new Joke('A kid threw a lump of cheddar at me', 'I thought "That\'s not very mature"')
-    ];
+    console.log('new - jokeViewChild is %s', this.jokeViewChild);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit - jokeViewChild is %s', this.jokeViewChild);
+
+    const jokes: JokeComponent[] = this.jokeViewChildren.toArray();
+    console.log(jokes);
+
+    console.log('ngAfterViewInit - headerEl is %s', this.headerEl);
+    this.headerEl.nativeElement.textContent = 'Best joke machine!';
+
+    console.log('ngAfterViewInit - jokeContentChild is %s', this.jokeContentChild);
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAFterContentInit - jokeContentChild is %s', this.jokeContentChild);
   }
 
   ngOnInit() {}
